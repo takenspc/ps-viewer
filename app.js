678e48338d33a3f42131faaef5a583abd329b63d
Takeshi Kurosawa <taken.spc@gmail.com>
2016-04-10 01:43:54 +0900

    Add CORS header for data.json

--- app.js
+++ app.js
@@ -39,7 +39,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+    setHeaders: (res, path, stat) => {
+        if (res.url === '/data/data.json') {
+            res.set('Access-Control-Allow-Origin', '*');
+        }
+    },
+}));
 
 app.use('/', routes);
 app.use('/status', status);
